431af8011fe1a6b88164f8051b03c34c2b6291f5
oswystan <oswystan@126.com>
2017-09-23 12:53:02 +0800

    feature: add new_mk

---
+++ new_mk/build/definations.mk
@@ -0,0 +1,29 @@
+#######################################################################
+##                     Copyright (C) 2017 wystan
+##
+##       filename: definations
+##    description: 
+##        created: 2017-09-23 11:26:56
+##         author: wystan
+## 
+#######################################################################
+
+define compile-c-file
+endef
+
+define compile-cpp-file
+endef
+
+define transform-o-to-static-lib
+endef
+
+define transform-o-to-shared-lib
+endef
+
+define transform-o-to-executable
+endef
+
+define transform-to-stripped
+endef
+
+#######################################################################
