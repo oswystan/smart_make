f0c36410a2c83af1f57a33edc9ef33a860d2a50f
oswystan <oswystan@126.com>
2017-09-27 15:15:22 +0800

    feature: add cpp lib

---
+++ new_mk/src/cpp_lib/module.mk
@@ -0,0 +1,12 @@
+###############################################################################
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_C_INCLUDES :=
+LOCAL_SRC_FILES := a.cpp
+LOCAL_MODULE := libcpp_shared
+
+include $(BUILD_SHARED_LIBRARY)
+
+###############################################################################
