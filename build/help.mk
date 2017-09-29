382e226f4034c312b4c524ad63317abd017eaca4
oswystan <oswystan@126.com>
2017-09-29 18:08:39 +0800

    feature: add help.mk

---
+++ build/help.mk
@@ -0,0 +1,23 @@
+#######################################################################
+##                     Copyright (C) 2017 wystan
+##
+##       filename: help
+##    description: 
+##        created: 2017-09-29 17:54:50
+##         author: wystan
+## 
+#######################################################################
+.PHONY: help
+
+help:
+	@echo 
+	@echo "SUB-COMMANDS:"
+	@echo "==========================================================="
+	@echo "    help         - show this."
+	@echo "    all          - compile all source code and install them."
+	@echo "    pkg          - make a xxx.tar.bz2 package for installation."
+	@echo "    clean        - rm -rf out/"
+	@echo "==========================================================="
+	@echo 
+
+#######################################################################
